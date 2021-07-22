46aafafbbf116c11239acbdbf26e209379b9ea5a
WillianSG <w.soares.girao@rug.nl>
2021-07-22 20:39:34 +0200

    returns magnitude change in percentage

--- helper_functions/magnitude_change_percentage.py
+++ helper_functions/magnitude_change_percentage.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 def magnitude_change_percentage(initial_val, final_val):
-	if final_val > initial_val:
-		magnitude_change = final_val - initial_val
-	else:
-		magnitude_change = initial_val - final_val
-		
-	return np.round(((magnitude_change*100)/initial_val), 1)
+    if initial_val == 0:
+        return final_val*100
+    else:
+    	if final_val > initial_val:
+    		magnitude_change = final_val - initial_val
+    	else:
+    		magnitude_change = initial_val - final_val
+    		
+    	return np.round(((magnitude_change*100)/initial_val), 1)
 
